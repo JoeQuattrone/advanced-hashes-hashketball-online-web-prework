bcf04d1f99c4c3d5675a682586218fbb500f7bb4
Joe Quattrone <joequattrone5@gmail.com>
2018-10-24 20:19:27 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -111,9 +111,8 @@ def player_numbers(team_name)
   game_hash.each do |home_away, team_data|
     if team_data[:team_name] == team_name
         return team_data[:players].map do|player, stats|
-
            stats[:number].to_i
-        end
+      end
     end
   end
 end
